afb5d1b6f30a447e6bca90c0f7d32ac58f2c0711
shound56 <patinosandoval.m@gmail.ccom>
2023-04-09 13:49:35 -0500

    Funcional

--- Proyecto_Tienda/src/Main.java
+++ Proyecto_Tienda/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        Producto.Nodo.ListasLigadas lista1= new Producto.Nodo.ListasLigadas();
+        Producto producto1 = new Producto("2023-05-30","Azucar","P0001",5000,"Azucarlite");
+        lista1.agregarProducto(producto1);
+        lista1.mostrarProducto();
     }
 }
--- Proyecto_Tienda/src/Producto.java
+++ Proyecto_Tienda/src/Producto.java
@@ -2,52 +2,94 @@ public class Producto {
     private String NombreProducto;
     private String Codigo;
     private String Vencimiento;
-    private double Costo;
     private double Precio;
     private String Marca;
-    public Producto(String Vencimiento, String NombreProducto, String Codigo, double Costo, double Precio, String Marca)
+    public Producto(String Vencimiento, String NombreProducto, String Codigo, double Precio, String Marca)
     {
         this.Vencimiento = Vencimiento ;
         this.Codigo = Codigo ;
         this.NombreProducto = NombreProducto ;
-        this.Costo = Costo ;
         this.Precio = Precio ;
         this.Marca = Marca ;
     }
-    public class Nodo {
-        private Producto producto;
-        private Nodo siguiente;
+    public String getNombreProducto(){
+        return NombreProducto;
+    }
+    public void  setNombreProducto(String NombreProducto){
+        this.NombreProducto= NombreProducto;
+    }
+    public String getVencimiento(){
+        return Vencimiento;
+    }
+    public void setVencimiento (String Vencimiento){
+        this.Vencimiento=Vencimiento;
+    }
+    public String getCodigo(){
+        return Codigo;
+    }
 
+    public void setCodigo(String Codigo) {
+        this.Codigo= Codigo;
+    }
+    public double getPrecio(){
+        return Precio;
+    }
+
+    public void setPrecio(double Precio) {
+        this.Precio = Precio;
+    }
+    public String getMarca(){
+        return Marca;
+    }
+
+    public void setMarca(String Marca) {
+       this.Marca = Marca;
+    }
+
+    public static class Nodo {
+        private Producto producto;
+        private Producto.Nodo siguiente;
         public Nodo(Producto producto, Nodo siguiente){
             this.producto = producto;
             this.siguiente = siguiente;
         }
-    }
-    public class ListasLigadas{
-        private Nodo cabeza;
-        public ListasLigadas(){
-            this.cabeza=null;
+        public Producto.Nodo getsiguiente(){
+            return siguiente;
         }
-        public void agregarProducto(Producto producto){
-            Nodo nuevoNodo = new Nodo(producto, null);
-            if (cabeza == null){
-                cabeza = nuevoNodo;
+
+        public void setsiguiente(Nodo siguiente) {
+            this.siguiente = siguiente;
+        }
+        public static class ListasLigadas{
+            private Nodo cabeza;
+            public  ListasLigadas(){
+                this.cabeza=null;
             }
-            else {
+            public void agregarProducto(Producto producto){
+               Nodo nuevoNodo = new Nodo (producto, null);
+                if (cabeza == null){
+                    cabeza = nuevoNodo;
+                }
+                else {
+                    Nodo actual = cabeza;
+
+                    while(actual.getsiguiente() != null){
+                        actual = actual.getsiguiente();
+                    }
+                    actual.setsiguiente(nuevoNodo);
+                }
+            }
+            public void mostrarProducto (){
                 Nodo actual = cabeza;
+                while (actual != null){
+                    System.out.println(actual.producto.getNombreProducto() + " " + actual.producto.getCodigo() + " " +
+                           actual.producto.getVencimiento() + " " + actual.producto.getPrecio() + " " + actual.producto.getMarca() );
+                    actual= actual.siguiente;
+                }
+            }
+        }
 
-            while(actual.siguiente != null){
-                actual = actual.siguiente;
-            }
-        }
-        }
-        public void mostrarProducto (){
-            Nodo actual = cabeza;
-            while (actual != null){
-                System.out.println(actual.siguiente);
-                actual= actual.siguiente;
-            }
-        }
     }
-
 }
+
+
